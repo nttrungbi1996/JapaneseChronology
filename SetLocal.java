d92cef23176da90c1de08e96d0b795e9162c9a6e
nttrungbi1996 <129488374+nttrungbi1996@users.noreply.github.com>
2023-07-27 18:39:16 +0900

    Add files via upload

---
+++ SetLocal.java
@@ -0,0 +1,34 @@
+package local.localdate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.chrono.JapaneseChronology;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class SetLocal {
+
+	public static String setLocalDate(LocalDate localDate) {
+
+		// 令和xx年xx月xx日 形式に変換
+		DateTimeFormatter japaneseEraDtf = DateTimeFormatter.ofPattern("Gyy年MM月dd日")
+				.withChronology(JapaneseChronology.INSTANCE)
+				.withLocale(Locale.JAPAN);
+
+		String formattedDate = localDate.format(japaneseEraDtf);
+
+		return formattedDate;
+	}
+
+	public static String setLocalDateTime(LocalDateTime localDateTime) {
+
+		// 令和xx年xx月xx日 形式に変換
+		DateTimeFormatter japaneseEraDtf = DateTimeFormatter.ofPattern("Gyy年MM月dd日HH時mm分")
+				.withChronology(JapaneseChronology.INSTANCE)
+				.withLocale(Locale.JAPAN);
+
+		String formattedDateTime = localDateTime.format(japaneseEraDtf);
+
+		return formattedDateTime;
+	}
+}
